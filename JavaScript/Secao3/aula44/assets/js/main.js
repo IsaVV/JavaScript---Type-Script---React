8621c9b4f44aae1edb1ad0004d02094a1fbdd086
Julio Moraes <jcrm@cesar.school>
2021-07-18 23:04:10 -0300

    Update

--- JavaScript/Secao3/aula44/assets/js/main.js
+++ JavaScript/Secao3/aula44/assets/js/main.js
@@ -1,21 +1,65 @@
 const form = document.querySelector('#formulario');
 
-form.addEventListener('submit', function (elemento){
-    elemento.preventDefault();
-    console.log('evento previnido');
-    setResultado('');
+form.addEventListener('submit', function (e) {
+    e.preventDefault();
+    const inputPeso = e.target.querySelector('#peso');
+    const inputAltura = e.target.querySelector('#altura');
+
+    const peso = Number(inputPeso.value);
+    const altura = Number(inputAltura.value);
+
+    if (!peso) {
+        setResultado('Peso inválido', false);
+        return;
+    }
+
+    if (!altura) {
+        setResultado('Altura inválida', false);
+        return;
+    }
+
+    const imc = getImc(peso, altura);
+    const nivelImc = getNivelImc(imc);
+
+    const msg = `Seu IMC é ${imc} (${nivelImc}).`;
+
+    setResultado(msg, true);
 });
 
+function getNivelImc (imc) {
+    const nivel = ['Abaixo do peso', 'Peso normal', 'Sobrepeso',
+        'Obesidade grau 1', 'Obesidade grau 2', 'Obesidade grau 3'];
+
+    if (imc >= 39.9) return nivel[5];
+    if (imc >= 34.9) return nivel[4];
+    if (imc >= 29.9) return nivel[3];
+    if (imc >= 24.9) return nivel[2];
+    if (imc >= 18.5) return nivel[1];
+    if (imc < 18.5) return nivel[0];
+}
+
+function getImc (peso, altura) {
+    const imc = peso / altura ** 2;
+    return imc.toFixed(2);
+}
+
 function criaP () {
-    const p = document.createElement('p');//cria um paragrafo
+    const p = document.createElement('p');//criando o elemento paragrafo
     return p;
 }
-function setResultado(msg){
+
+function setResultado (msg, isValid) {
     const resultado = document.querySelector('#resultado');
-    resultado.innerHTML = ``;
-    const p = criaP();
-    resultado.appendChild();//coloca um filho dentro da div de resultado
+    resultado.innerHTML = '';
+
+    const p = criaP();//cria o elemento paragrafo
+
+    if (isValid) {
+        p.classList.add('paragrafo-resultado');//adiciona uma classe ao p
+    } else {
+        p.classList.add('bad');//adiciona uma classe ao p
+    }
+
+    p.innerHTML = msg; //coloca a msg recebida dentro do html de p 
+    resultado.appendChild(p);//coloca p dentro de resultado como filho 
 }
-
-
-
